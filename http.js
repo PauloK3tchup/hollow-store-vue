638f1961c6638a5b5d9f5fcdcbe57e77ea63a0c0
PauloK3tchup <passandoalimpo13@gmail.com>
2025-11-27 10:15:12 -0300

    HollowStoreDeploy

--- http.js
+++ http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/'
+const baseURL = import.meta.env.VITE_API_URL || 'https://hollowstore-backend.onrender.com/api/'
 
 const http = axios.create({
   baseURL,
